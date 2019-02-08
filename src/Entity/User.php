22488d02674751d2055d3925bee2be236e569828
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:18 -0500

    [tuts-apply-diff] Applying existing diff file for step regvalidation-add-constraints

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -23,6 +24,8 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Groups("main")
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
     private $email;
 
