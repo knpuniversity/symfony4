0b5cb9c3aaf7b28bbb77bc51fc4008a747626874
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:37 -0500

    [tuts-apply-diff] Applying existing diff file for step custom-di-use-bind-under-defaults

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -12,11 +12,11 @@ class MarkdownHelper
     private $markdown;
     private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
-        $this->logger = $logger;
+        $this->logger = $markdownLogger;
     }
 
     public function parse(string $source): string
