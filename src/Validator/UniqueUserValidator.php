bc14d0b5b47113d4bdb2339502caf00d9ec4b09a
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:05:03 -0500

    [tuts-apply-diff] Applying existing diff file for step custom-constraint-make-the-query

--- src/Validator/UniqueUserValidator.php
+++ src/Validator/UniqueUserValidator.php
@@ -2,13 +2,29 @@
 
 namespace App\Validator;
 
+use App\Repository\UserRepository;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class UniqueUserValidator extends ConstraintValidator
 {
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function validate($value, Constraint $constraint)
     {
+        $existingUser = $this->userRepository->findOneBy([
+            'email' => $value
+        ]);
+
+        if (!$existingUser) {
+            return;
+        }
+
         /* @var $constraint \App\Validator\UniqueUser */
 
         $this->context->buildViolation($constraint->message)
