1d95d7a2389a88efebe8795bb1cb4733a06edf2e
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:53 -0500

    [tuts-apply-diff] Applying existing diff file for step options-get-dump-options-data

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -23,7 +23,8 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        dd($options);
+        $article = $options['data'] ?? null;
+        dd($article);
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!'
