1d69fe1f9010cec652091bb02e7ce2f60eaa11c0
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:00:48 -0500

    [tuts-apply-diff] Applying existing diff file for step extensions-update-migration-for-nullable

--- src/Migrations/Version20180418130337.php
+++ src/Migrations/Version20180418130337.php
@@ -15,7 +15,8 @@ class Version20180418130337 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE article ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE article SET created_at = NOW(), updated_at = NOW()');
     }
 
     public function down(Schema $schema)
