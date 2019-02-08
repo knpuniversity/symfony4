7ee1e10f31bdca58166bfb58a913170fa4ada98e
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:55 -0500

    [tuts-apply-diff] Applying existing diff file for step user-add-firstname-field

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,6 +27,11 @@ class User implements UserInterface
      */
     private $roles = [];
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -96,5 +101,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
     }
 }
