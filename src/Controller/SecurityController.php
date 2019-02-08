0bd849471ca44ba6d98d19b9f52197f000b84559
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:03 -0500

    [tuts-apply-diff] Applying existing diff file for step login-form-copy-controller-template

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,16 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('security/login.html.twig', [
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
 
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ]);
     }
 }
