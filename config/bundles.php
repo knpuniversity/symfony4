5c9651a5447b40ee4f682832e2c4809f033b5bf6
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:59 -0500

    [tuts-apply-diff] Applying existing diff file for step maker-install-maker

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
