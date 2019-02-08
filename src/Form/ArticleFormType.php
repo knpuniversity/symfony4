10af162e06e8a099d7bac2ef788fa2eec7299928
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:57 -0500

    [tuts-apply-diff] Applying existing diff file for step entitytype-getchoices-with-custom-query

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use App\Entity\User;
+use App\Repository\ArticleRepository;
+use App\Repository\UserRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -12,6 +14,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -27,7 +36,8 @@ class ArticleFormType extends AbstractType
                 'choice_label' => function(User $user) {
                     return sprintf('(%d) %s', $user->getId(), $user->getEmail());
                 },
-                'placeholder' => 'Choose an author'
+                'placeholder' => 'Choose an author',
+                'choices' => $this->userRepository->findAllEmailAlphabetical(),
             ])
         ;
     }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,6 +19,18 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
+    /**
+     * @return User[]
+     */
+    public function findAllEmailAlphabetical()
+    {
+        return $this->createQueryBuilder('u')
+            ->orderBy('u.email', 'ASC')
+            ->getQuery()
+            ->execute()
+        ;
+    }
+
 //    /**
 //     * @return User[] Returns an array of User objects
 //     */
