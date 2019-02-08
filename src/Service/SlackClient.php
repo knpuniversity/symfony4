7f5c42278c68fee87204dfb8a2e855c205671b29
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:56 -0500

    [tuts-apply-diff] Applying existing diff file for step setter-di-adding-setlogger

--- src/Service/SlackClient.php
+++ src/Service/SlackClient.php
@@ -3,18 +3,33 @@
 namespace App\Service;
 
 use Nexy\Slack\Client;
+use Psr\Log\LoggerInterface;
 
 class SlackClient
 {
     private $slack;
+
+    /**
+     * @var LoggerInterface|null
+     */
+    private $logger;
 
     public function __construct(Client $slack)
     {
         $this->slack = $slack;
     }
 
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function sendMessage(string $from, string $message)
     {
+        if ($this->logger) {
+            $this->logger->info('Beaming a message to Slack!');
+        }
+
         $message = $this->slack->createMessage()
             ->from($from)
             ->withIcon(':ghost:')
