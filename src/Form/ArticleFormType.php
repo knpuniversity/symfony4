65146c530240498ac97203b6544065cd894e870f
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:52 -0500

    [tuts-apply-diff] Applying existing diff file for step entitytype-pass-class-option

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,7 +22,9 @@ class ArticleFormType extends AbstractType
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
             ])
-            ->add('author', EntityType::class)
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+            ])
         ;
     }
 
