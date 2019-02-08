15ecad84788d81e026ac51e473b4e238aeb5d089
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:01 -0500

    [tuts-apply-diff] Applying existing diff file for step validation-notblank

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
@@ -26,6 +27,7 @@ class Article
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $title;
 
