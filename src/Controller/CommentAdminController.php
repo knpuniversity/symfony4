491aa4cac3649606691b65dcc235f048478ebb8c
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:34 -0500

    [tuts-apply-diff] Applying existing diff file for step join-hook-up-search-logic

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,9 +12,10 @@ class CommentAdminController extends Controller
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(CommentRepository $repository)
+    public function index(CommentRepository $repository, Request $request)
     {
-        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+        $q = $request->query->get('q');
+        $comments = $repository->findAllWithSearch($q);
 
         return $this->render('comment_admin/index.html.twig', [
             'comments' => $comments,
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -28,6 +28,27 @@ class CommentRepository extends ServiceEntityRepository
         ;
     }
 
+    /**
+     * @param string|null $term
+     * @return Comment[]
+     */
+    public function findAllWithSearch(?string $term)
+    {
+        $qb = $this->createQueryBuilder('c');
+
+        if ($term) {
+            $qb->andWhere('c.content LIKE :term OR c.authorName LIKE :term')
+                ->setParameter('term', '%' . $term . '%')
+            ;
+        }
+
+        return $qb
+            ->orderBy('c.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
 //    /**
 //     * @return Comment[] Returns an array of Comment objects
 //     */
