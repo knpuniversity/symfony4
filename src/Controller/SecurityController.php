0252d826c955dbba5a4aa33ebd974e50fd191d00
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:01 -0500

    [tuts-apply-diff] Applying existing diff file for step login-form-make-controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/security", name="security")
+     */
+    public function index()
+    {
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
+        ]);
+    }
+}
