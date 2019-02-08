75629d069c2feccce9b6927e2287779e80ca708c
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:58:51 -0500

    [tuts-apply-diff] Applying existing diff file for step profiler-dump-in-twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -26,7 +26,6 @@ class ArticleController extends AbstractController
             'Woohoo! I\'m going on an all-asteroid diet!',
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
-        dump($slug, $this);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
