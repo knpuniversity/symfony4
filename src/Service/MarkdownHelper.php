8cfc974d65ed418a4a6b896cc6df092a19dc0099
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:41 -0500

    [tuts-apply-diff] Applying existing diff file for step parameters-try-to-inject-isdebug

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -11,12 +11,14 @@ class MarkdownHelper
     private $cache;
     private $markdown;
     private $logger;
+    private $isDebug;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
 
     public function parse(string $source): string
@@ -25,7 +27,11 @@ class MarkdownHelper
             $this->logger->info('They are talking about bacon again!');
         }
 
-        dump($this->cache);die;
+        // skip caching entirely in debug
+        if ($this->isDebug) {
+            return $this->markdown->transform($source);
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
