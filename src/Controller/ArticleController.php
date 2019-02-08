89739a790797345d30570b4b8835b5d8c3ba92ce
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:58:38 -0500

    [tuts-apply-diff] Applying existing diff file for step pages-convert-route-to-annotation

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
