d76a86ce18d930d0cea48fdfcdd6621f08ab645d
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:46 -0500

    [tuts-apply-diff] Applying existing diff file for step options-set-help-option

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'help' => 'Choose something catchy!'
+            ])
             ->add('content')
         ;
     }
