d373eb48d2a09dc09fb6a8d81d65f82969cd933a
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:38 -0500

    [tuts-apply-diff] Applying existing diff file for step customtype-add-finder-callback-option

--- src/Form/DataTransformer/EmailToUserTransformer.php
+++ src/Form/DataTransformer/EmailToUserTransformer.php
@@ -10,10 +10,12 @@ use Symfony\Component\Form\Exception\TransformationFailedException;
 class EmailToUserTransformer implements DataTransformerInterface
 {
     private $userRepository;
+    private $finderCallback;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, callable $finderCallback)
     {
         $this->userRepository = $userRepository;
+        $this->finderCallback = $finderCallback;
     }
 
     public function transform($value)
@@ -35,7 +37,8 @@ class EmailToUserTransformer implements DataTransformerInterface
             return;
         }
 
-        $user = $this->userRepository->findOneBy(['email' => $value]);
+        $callback = $this->finderCallback;
+        $user = $callback($this->userRepository, $value);
 
         if (!$user) {
             throw new TransformationFailedException(sprintf('No user found with email "%s"', $value));
--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -20,7 +20,10 @@ class UserSelectTextType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->addModelTransformer(new EmailToUserTransformer($this->userRepository));
+        $builder->addModelTransformer(new EmailToUserTransformer(
+            $this->userRepository,
+            $options['finder_callback']
+        ));
     }
 
     public function getParent()
@@ -32,6 +35,9 @@ class UserSelectTextType extends AbstractType
     {
         $resolver->setDefaults([
             'invalid_message' => 'Hmm, user not found!',
+            'finder_callback' => function(UserRepository $userRepository, string $email) {
+                return $userRepository->findOneBy(['email' => $email]);
+            }
         ]);
     }
 }
