e25bcf7bc11e32485ffe0bc641be914ac6085891
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:58:42 -0500

    [tuts-apply-diff] Applying existing diff file for step twig-install-twig

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
