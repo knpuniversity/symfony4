2be18d4b01f3a725e5ba25f1b16bbc8957d051c4
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:58:44 -0500

    [tuts-apply-diff] Applying existing diff file for step twig-loop-over-comments

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -21,8 +21,15 @@ class ArticleController extends AbstractController
      */
     public function show($slug)
     {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
         ]);
     }
 }
