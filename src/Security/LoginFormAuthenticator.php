5bdc2fdbc33ddf4b0e1c67a93dd4d05be6804350
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:09 -0500

    [tuts-apply-diff] Applying existing diff file for step authenticator-use-abstractformloginauthenticator

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -4,12 +4,11 @@ namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 
-class LoginFormAuthenticator extends AbstractGuardAuthenticator
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
     public function supports(Request $request)
     {
@@ -31,23 +30,13 @@ class LoginFormAuthenticator extends AbstractGuardAuthenticator
         // todo
     }
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
-    {
-        // todo
-    }
-
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         // todo
     }
 
-    public function start(Request $request, AuthenticationException $authException = null)
+    protected function getLoginUrl()
     {
-        // todo
-    }
-
-    public function supportsRememberMe()
-    {
-        // todo
+        // TODO: Implement getLoginUrl() method.
     }
 }
