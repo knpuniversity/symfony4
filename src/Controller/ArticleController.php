2e967ff516014c77f4fe72006b1ebd029b75de6a
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:23 -0500

    [tuts-apply-diff] Applying existing diff file for step config-dump-markdown

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -58,6 +58,8 @@ EOF;
         }
         $articleContent = $item->get();
 
+        dump($markdown);die;
+
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
             'slug' => $slug,
