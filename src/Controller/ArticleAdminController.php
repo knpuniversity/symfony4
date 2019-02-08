250e7044ef55773c7a7d414d629358d313777467
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:39 -0500

    [tuts-apply-diff] Applying existing diff file for step first-form-process

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -7,6 +7,7 @@ use App\Form\ArticleFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,9 +17,14 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/new", name="admin_article_new")
      * @IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getData());
+        }
 
         return $this->render('article_admin/new.html.twig', [
             'articleForm' => $form->createView()
