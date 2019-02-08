d1faf75e2b5c02ca096d9be79979fd103e8c537f
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:11 -0500

    [tuts-apply-diff] Applying existing diff file for step fixtures-mostly-create-100-comments

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -2,14 +2,21 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class CommentFixture extends BaseFixture
 {
     protected function loadData(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->createMany(Comment::class, 100, function(Comment $comment) {
+            $comment->setContent(
+                $this->faker->boolean ? $this->faker->paragraph : $this->faker->sentences(2, true)
+            );
+
+            $comment->setAuthorName($this->faker->name);
+            $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
+        });
 
         $manager->flush();
     }
