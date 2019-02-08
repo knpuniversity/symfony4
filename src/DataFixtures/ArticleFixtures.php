ac649b39a1e6415d2370370f31ee440ea39c5890
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:23 -0500

    [tuts-apply-diff] Applying existing diff file for step voters-update-article-fixture

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -22,11 +22,6 @@ class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
         'lightspeed.png',
     ];
 
-    private static $articleAuthors = [
-        'Mike Ferengi',
-        'Amy Oort',
-    ];
-
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(10, 'main_articles', function($count) use ($manager) {
@@ -57,7 +52,7 @@ EOF
                 $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
 
-            $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
+            $article->setAuthor($this->getRandomReference('main_users'))
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages))
             ;
@@ -77,6 +72,7 @@ EOF
     {
         return [
             TagFixture::class,
+            UserFixture::class,
         ];
     }
 }
