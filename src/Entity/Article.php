b5a7caf92e2d5bbbdffe6f27bc5ae63bb83ffd1d
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:05:13 -0500

    [tuts-apply-diff] Applying existing diff file for step formevent-empty-out-specific-location

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -287,6 +287,10 @@ class Article
     {
         $this->location = $location;
 
+        if (!$this->location || $this->location === 'interstellar_space') {
+            $this->setSpecificLocationName(null);
+        }
+
         return $this;
     }
 
