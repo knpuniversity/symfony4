2d9085afbab82941484dc1a87268b6f297e5a2d6
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:51 -0500

    [tuts-apply-diff] Applying existing diff file for step formtypeextension-override-rows-option

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -27,7 +27,9 @@ class ArticleFormType extends AbstractType
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!'
             ])
-            ->add('content')
+            ->add('content', null, [
+                'rows' => 15
+            ])
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
             ])
