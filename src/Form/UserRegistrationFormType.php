44e64aab712eae56d9f195a02ecc8b339f661a33
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:20 -0500

    [tuts-apply-diff] Applying existing diff file for step regvalidation-plainpassword-constraints

--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserRegistrationFormType extends AbstractType
 {
@@ -17,7 +19,16 @@ class UserRegistrationFormType extends AbstractType
             // don't use password: avoid EVER setting that on a
             // field that might be persisted
             ->add('plainPassword', PasswordType::class, [
-                'mapped' => false
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Choose a password!'
+                    ]),
+                    new Length([
+                        'min' => 5,
+                        'minMessage' => 'Come on, you can think of a password longer than that!'
+                    ])
+                ]
             ]);
         ;
     }
