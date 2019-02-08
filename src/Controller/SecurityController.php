cfffd121ecc5616aa7196d23fd8b9b4a67f1e06c
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:18 -0500

    [tuts-apply-diff] Applying existing diff file for step logout-adding-logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -24,4 +24,12 @@ class SecurityController extends AbstractController
             'error'         => $error,
         ]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('Will be intercepted before getting here');
+    }
 }
