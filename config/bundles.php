413c9fe8f15518fbb7f6f685f5bd180020e144b0
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:00:18 -0500

    [tuts-apply-diff] Applying existing diff file for step twigextension-install-knptimebundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
