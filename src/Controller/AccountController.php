594c829b1413a0d560c5ae4da66c9d0daa5733c5
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:33 -0500

    [tuts-apply-diff] Applying existing diff file for step roles-add-accountcontroller

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountController extends AbstractController
+{
+    /**
+     * @Route("/account", name="account")
+     */
+    public function index()
+    {
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+}
