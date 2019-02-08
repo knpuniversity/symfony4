6759f528981590bfdcc7ad7400291592de9f1689
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:10 -0500

    [tuts-apply-diff] Applying existing diff file for step javascript-hook-up-ajax

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
         var $link = $(e.currentTarget);
         $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
 
-        $('.js-like-article-count').html('TEST');
+        $.ajax({
+            method: 'POST',
+            url: $link.attr('href')
+        }).done(function(data) {
+            $('.js-like-article-count').html(data.hearts);
+        })
     });
 });
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -30,6 +30,7 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
     }
