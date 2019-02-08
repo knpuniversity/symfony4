fd8e88139e753889aff4ee9a3237f91b6ca51471
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:39 -0500

    [tuts-apply-diff] Applying existing diff file for step is-granted-conditionally-show-admin-link

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -4,14 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class ArticleAdminController extends AbstractController
 {
     /**
-     * @Route("/admin/article/new")
+     * @Route("/admin/article/new", name="admin_article_new")
      */
     public function new(EntityManagerInterface $em)
     {
