013c3c905a9d3f3ad9d31cb066080665a050dfe7
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:36 -0500

    [tuts-apply-diff] Applying existing diff file for step first-form-create-articleformtype

---
+++ src/Form/ArticleFormType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ArticleFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        
+    }
+}
