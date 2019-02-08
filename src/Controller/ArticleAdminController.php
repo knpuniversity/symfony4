6cf0a63550477589784c5b7baca74fd94d9c6fc8
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:40 -0500

    [tuts-apply-diff] Applying existing diff file for step first-form-save-article

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -23,7 +23,16 @@ class ArticleAdminController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
+            $data = $form->getData();
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_homepage');
         }
 
         return $this->render('article_admin/new.html.twig', [
