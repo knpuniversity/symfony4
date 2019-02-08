c8e3fa342c34a8450b8964e8ec89143c68a14595
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:34 -0500

    [tuts-apply-diff] Applying existing diff file for step customtype-add-transform

--- src/Form/DataTransformer/EmailToUserTransformer.php
+++ src/Form/DataTransformer/EmailToUserTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Form\DataTransformer;
 
+use App\Entity\User;
 use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Exception\TransformationFailedException;
 
@@ -9,7 +10,15 @@ class EmailToUserTransformer implements DataTransformerInterface
 {
     public function transform($value)
     {
-        dd('transform', $value);
+        if (null === $value) {
+            return '';
+        }
+
+        if (!$value instanceof User) {
+            throw new \LogicException('The UserSelectTextType can only be used with User objects');
+        }
+
+        return $value->getEmail();
     }
 
     public function reverseTransform($value)
