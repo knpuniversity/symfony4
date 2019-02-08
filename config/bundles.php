d1487fe25aa2ebda4454e7cc2e5c2c659735cb1a
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:58:49 -0500

    [tuts-apply-diff] Applying existing diff file for step profiler-install-profiler

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
