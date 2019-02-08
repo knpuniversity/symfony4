da786fb46a67f31fc9aced1be9aff0d898f129ff
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:12 -0500

    [tuts-apply-diff] Applying existing diff file for step registration-make-form

---
+++ src/Form/UserRegistrationFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserRegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('firstName')
+            ->add('password')
+            ->add('twitterUsername')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
