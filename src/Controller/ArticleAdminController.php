91eb7586960615e3cf8e3343173d1f0655e8c12e
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:03:45 -0500

    [tuts-apply-diff] Applying existing diff file for step data-class-save-entity

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -24,11 +24,8 @@ class ArticleAdminController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            dd($data);
-            $article = new Article();
-            $article->setTitle($data['title']);
-            $article->setContent($data['content']);
+            /** @var Article $article */
+            $article = $form->getData();
             $article->setAuthor($this->getUser());
 
             $em->persist($article);
