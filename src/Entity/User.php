7c0b1ef7b9316369b0e42feb25d27b8a3bfb54e3
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:19 -0500

    [tuts-apply-diff] Applying existing diff file for step regvalidation-uniqueentity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,17 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *     fields={"email"},
+ *     message="I think you're already registered!"
+ * )
  */
 class User implements UserInterface
 {
