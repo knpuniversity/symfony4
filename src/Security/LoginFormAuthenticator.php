46ca40d3379f0b0e53ded4b8d2f8ebd13c565c5c
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:17 -0500

    [tuts-apply-diff] Applying existing diff file for step authenticator-set-use-last-username

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -31,10 +32,17 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function getCredentials(Request $request)
     {
-        return [
+        $credentials = [
             'email' => $request->request->get('email'),
             'password' => $request->request->get('password'),
         ];
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME,
+            $credentials['email']
+        );
+
+        return $credentials;
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
