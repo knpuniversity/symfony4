9e5baf5e983dd0e07f6698087804046be507e921
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:40 -0500

    [tuts-apply-diff] Applying existing diff file for step customtype-setup-endpoint

--- src/Controller/AdminUtilityController.php
+++ src/Controller/AdminUtilityController.php
@@ -2,9 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 class AdminUtilityController extends AbstractController
 {
+    /**
+     * @Route("/admin/utility/users", methods="GET")
+     */
+    public function getUsersApi(UserRepository $userRepository)
+    {
+        $users = $userRepository->findAllEmailAlphabetical();
 
+        return $this->json([
+            'users' => $users
+        ], 200, [], ['groups' => ['main']]);
+    }
 }
