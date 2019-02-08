e5331c758510bef3efa7b6a1548a7298622e24ba
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:54 -0500

    [tuts-apply-diff] Applying existing diff file for step options-disable-author-on-edit

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -24,7 +24,8 @@ class ArticleFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $article = $options['data'] ?? null;
-        dd($article);
+        $isEdit = $article && $article->getId();
+
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!'
@@ -35,7 +36,9 @@ class ArticleFormType extends AbstractType
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
             ])
-            ->add('author', UserSelectTextType::class)
+            ->add('author', UserSelectTextType::class, [
+                'disabled' => $isEdit
+            ])
         ;
     }
 
