3821e890e1d7604413f52a48102e0996d3d252e8
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:00:34 -0500

    [tuts-apply-diff] Applying existing diff file for step updates-make-heart-endpoint-real

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -62,12 +62,13 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    public function toggleArticleHeart(Article $article, LoggerInterface $logger, EntityManagerInterface $em)
     {
-        // TODO - actually heart/unheart the article!
+        $article->setHeartCount($article->getHeartCount() + 1);
+        $em->flush();
 
         $logger->info('Article is being hearted!');
 
-        return new JsonResponse(['hearts' => rand(5, 100)]);
+        return new JsonResponse(['hearts' => $article->getHeartCount()]);
     }
 }
