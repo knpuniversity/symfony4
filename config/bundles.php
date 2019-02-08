91a01cd08d031a4d62dd3d8ba5153d7cbdb93278
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:00:45 -0500

    [tuts-apply-diff] Applying existing diff file for step extensions-install-stofdoctrine

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
