2dc379b6e4d13476956ded861bb0aeec3aedfb24
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:15 -0500

    [tuts-apply-diff] Applying existing diff file for step fixture-ordering-rename-back

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -7,7 +7,7 @@ use App\Entity\Comment;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class A0CommentFixture extends BaseFixture implements DependentFixtureInterface
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
