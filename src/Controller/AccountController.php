f5d87159d50de2c0a1a46da7cd87bc54df4df919
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:35 -0500

    [tuts-apply-diff] Applying existing diff file for step roles-protect-account-with-role-user

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_USER")
+ */
 class AccountController extends AbstractController
 {
     /**
