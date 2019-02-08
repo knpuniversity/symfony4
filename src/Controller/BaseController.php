d68e315f86f77eec36bac08e6c8b47fa7e87940a
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:05:15 -0500

    [tuts-apply-diff] Applying existing diff file for step formevent-fix-basecontroller

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,10 +5,9 @@ namespace App\Controller;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @method User getUser()
+ */
 abstract class BaseController extends AbstractController
 {
-    protected function getUser(): User
-    {
-        return parent::getUser();
-    }
 }
