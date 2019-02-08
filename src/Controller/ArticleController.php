8aa6dffd33288f24baeaba5bebb0e70b39264b8b
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:05 -0500

    [tuts-apply-diff] Applying existing diff file for step links-try-to-link-to-show-page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -17,7 +17,7 @@ class ArticleController extends AbstractController
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
