b741634cfbff045f8e4139fa6762f81079db9dad
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:54 -0500

    [tuts-apply-diff] Applying existing diff file for step user-installing-security

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
