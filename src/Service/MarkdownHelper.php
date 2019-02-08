ca6ab9b6fe7ce2b2d56b75a24a76ddac6048f8d0
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 09:59:33 -0500

    [tuts-apply-diff] Applying existing diff file for step custom-di-di-normal-logger

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,21 +3,28 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
+    private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking about bacon again!');
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
