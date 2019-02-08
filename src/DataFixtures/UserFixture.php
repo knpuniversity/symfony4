7d8db507e49d441c67d3c0c89ca6c456479c1a23
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:02:00 -0500

    [tuts-apply-diff] Applying existing diff file for step user-fill-in-fixture

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,14 +2,20 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class UserFixture extends BaseFixture
 {
     protected function loadData(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->createMany(10, 'main_users', function($i) {
+            $user = new User();
+            $user->setEmail(sprintf('spacebar%d@example.com', $i));
+            $user->setFirstName($this->faker->firstName);
+
+            return $user;
+        });
 
         $manager->flush();
     }
