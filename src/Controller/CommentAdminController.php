24579a5924785c88492c33f9d2a1e7eed44f0376
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:25 -0500

    [tuts-apply-diff] Applying existing diff file for step comment-admin-make-admincommentcontroller

---
+++ src/Controller/CommentAdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CommentAdminController extends Controller
+{
+    /**
+     * @Route("/comment/admin", name="comment_admin")
+     */
+    public function index()
+    {
+        return $this->render('comment_admin/index.html.twig', [
+            'controller_name' => 'CommentAdminController',
+        ]);
+    }
+}
