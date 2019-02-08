fca87cad59e8854ed9f17392a73b2924eeacb4d1
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:31 -0500

    [tuts-apply-diff] Applying existing diff file for step comment-admin-print-table

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,10 +11,12 @@ class CommentAdminController extends Controller
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index()
+    public function index(CommentRepository $repository)
     {
+        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
