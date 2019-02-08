54bf21261165162224999d4b1ec5e195eed4f2b7
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:00:16 -0500

    [tuts-apply-diff] Applying existing diff file for step twigextension-process-through-markdown

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,12 +2,20 @@
 
 namespace App\Twig;
 
+use App\Service\MarkdownHelper;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
+    private $markdownHelper;
+
+    public function __construct(MarkdownHelper $markdownHelper)
+    {
+        $this->markdownHelper = $markdownHelper;
+    }
+
     public function getFilters(): array
     {
         return [
@@ -17,6 +25,6 @@ class AppExtension extends AbstractExtension
 
     public function processMarkdown($value)
     {
-        return strtoupper($value);
+        return $this->markdownHelper->parse($value);
     }
 }
