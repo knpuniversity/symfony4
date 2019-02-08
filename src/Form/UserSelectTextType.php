fbf088a5091234f00db53a9c8da047844c5d7290
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:04:36 -0500

    [tuts-apply-diff] Applying existing diff file for step customtype-default-invalid-message

--- src/Form/UserSelectTextType.php
+++ src/Form/UserSelectTextType.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserSelectTextType extends AbstractType
 {
@@ -25,5 +26,12 @@ class UserSelectTextType extends AbstractType
     public function getParent()
     {
         return TextType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'invalid_message' => 'Hmm, user not found!',
+        ]);
     }
 }
