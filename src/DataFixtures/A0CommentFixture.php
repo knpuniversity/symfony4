4edd6c497ae19fa3b9bba1dc54ec073fbe0a8fc9
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:01:15 -0500

    [tuts-apply-diff] Applying existing diff file for step fixture-ordering-create-a0commentfixture

--- src/DataFixtures/A0CommentFixture.php
+++ src/DataFixtures/A0CommentFixture.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CommentFixture extends BaseFixture
+class A0CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
     protected function loadData(ObjectManager $manager)
     {
@@ -21,5 +22,10 @@ class CommentFixture extends BaseFixture
         });
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [ArticleFixtures::class];
     }
 }
