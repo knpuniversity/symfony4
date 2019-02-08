8cc0e09ff8429b4cbe313f01ac6283c68873244f
Ryan Weaver <ryan@thatsquality.com>
2019-02-08 10:00:47 -0500

    [tuts-apply-diff] Applying existing diff file for step extensions-add-createdat-updatedat

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -53,6 +53,16 @@ class Article
      */
     private $imageFilename;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
     public function getId()
     {
         return $this->id;
@@ -153,5 +163,29 @@ class Article
     public function getImagePath()
     {
         return 'images/'.$this->getImageFilename();
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
     }
 }
