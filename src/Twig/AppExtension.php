72177a3e4b0df618bf0a8ddcc558bd0eec35c3fd
Ryan Weaver <ryan@thatsquality.com>
2019-02-21 09:07:47 -0500

    Updating deprecated class

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,7 +4,7 @@ namespace App\Twig;
 
 use App\Service\MarkdownHelper;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
